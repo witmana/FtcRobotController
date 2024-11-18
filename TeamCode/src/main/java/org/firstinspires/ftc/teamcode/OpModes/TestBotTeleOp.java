98668f6e966a00a7a3a91dffcb3412ac34ee872e
witmana <35934048+witmana@users.noreply.github.com>
2024-11-18 13:56:43 -0800

    Reapply "changed teleop names"
    This reverts commit fb3ee07aa4107b69f074c9adbebcd4171a23e4b3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestBotTeleOp.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Subsystems.RobotHardware;
 import org.firstinspires.ftc.teamcode.Subsystems.TestBotHardware;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TestBotTeleOp", group="Linear OpMode")
+//@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TestBotTeleOp", group="Linear OpMode")
 public class TestBotTeleOp extends LinearOpMode {
 
     //declaring instance of robot hardware
